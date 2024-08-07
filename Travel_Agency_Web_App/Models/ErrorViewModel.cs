6c08e2d370e5eabf2dd8c94ad71370ca2b3e236e
yumalieser <216051010@bingol.edu.tr>
2024-08-07 15:56:53 +0300

    Upload Begin Project Files

---
+++ Travel_Agency_Web_App/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Travel_Agency_Web_App.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
